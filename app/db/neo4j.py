5442607e7575c3c9b1b7b7b07b62e5076ef05850
Mauro Aguilera <mauroaguilera98@gmail.com>
2026-02-21 20:46:34 -0300

    Mejora del rendimiento asíncrono y el sistema de caché para el manejo de calificaciones.
    Convierte las operaciones de Neo4j a asíncronas, elimina el uso de threadpool y agrega almacenamiento en caché en Redis de los hashes de auditoría para minimizar la latencia de Cassandra. Actualiza el script de inserción masiva para compatibilidad y mayor eficiencia con la API. Actualiza dependencias para mejorar la confiabilidad y el soporte asíncrono.

--- app/db/neo4j.py
+++ app/db/neo4j.py
@@ -1,10 +1,10 @@
+from neo4j import AsyncGraphDatabase
 import os
-from neo4j import GraphDatabase
 
-# En Docker, usamos el nombre del servicio 'neo4j' en lugar de 'localhost'
-NEO4J_HOST = os.getenv("NEO4J_HOST", "neo4j")
-NEO4J_URI = f"bolt://{NEO4J_HOST}:7687"
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+# Pon aquí la contraseña que usaron tus compañeros
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password") 
 
-driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+# El driver ahora es asíncrono
+driver = AsyncGraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
