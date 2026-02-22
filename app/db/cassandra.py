8899e047bf3a05bb1d21a78f5086cabe4e1d9c5b
Maximiliano Lovato <116845565+rockhex@users.noreply.github.com>
2026-02-22 18:57:03 -0300

    correcciciones

--- app/db/cassandra.py
+++ app/db/cassandra.py
@@ -1,6 +1,5 @@
 from cassandra.cluster import Cluster
 import os
-# cluster = Cluster(["127.0.0.1"])
  
 
 CASSANDRA_HOST = os.getenv("CASSANDRA_HOST", "localhost")
@@ -16,15 +15,7 @@ WITH replication = {'class':'SimpleStrategy','replication_factor':1}
 
 session.set_keyspace("edugrade")
 
-# session.execute("""
-# CREATE TABLE IF NOT EXISTS audit_log (
-#     student_id text,
-#     event_time timestamp,
-#     action text,
-#     hash text,
-#     PRIMARY KEY (student_id, event_time)
-# ) WITH CLUSTERING ORDER BY (event_time DESC)
-# """)
+
 
 session.execute("""
 CREATE TABLE IF NOT EXISTS audit_log (
